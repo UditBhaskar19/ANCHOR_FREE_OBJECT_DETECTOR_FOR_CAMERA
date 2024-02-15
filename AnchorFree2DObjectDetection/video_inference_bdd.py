8b49ab9c3fc1f0559fe151dacefeb73bc008b0f6
Udit Bhaskar <udit.bhaskar.talukdar@gmail.com>
2024-02-15 07:22:17 +0530

    update project

--- AnchorFree2DObjectDetection/video_inference_bdd.py
+++ AnchorFree2DObjectDetection/video_inference_bdd.py
@@ -10,26 +10,17 @@ from modules.first_stage.set_parameters_for_inference import set_param_for_video
 
 _INTERPOLATION_MODE_ = cv2.INTER_AREA
 
-video_dir1 = 'D:/Downloads/bdd100k_videos_test_00/bdd100k/videos/test/'
-clip1 = 'caeb0fbb-410c10ca.mov'
-clip2 = 'cae23877-a1826db4.mov'
-clip3 = 'cadf77c3-2857db38.mov'
-clip4 = 'cad03998-e6cc2ae6.mov'
-clip5 = 'cad180c4-2edc9315.mov'
-clip6 = 'cad180c4-d05b49f1.mov'
-clip7 =  'cabc30fc-eb673c5a.mov' # '01621265-578a97d7.mov'
+IN_VIDEO_DIR = 'dataset/bdd/video_clips'
+clip1 = '01621265-578a97d7.mov'
+clip2 = '02191150-4615d36f.mov'
+clip3 = 'cabc30fc-eb673c5a.mov'
+clip4 = 'cad180c4-d05b49f1.mov'
 
-clip1b =  '01621265-578a97d7.mov'
-video_dir2 = 'D:/Downloads/bdd100k_videos_train_00/bdd100k/videos/train'
-
-video_file = clip4
-video_path = os.path.join(video_dir1, video_file)
-
-# video_file = clip1b
-# video_path = os.path.join(video_dir2, video_file)
+video_file = clip1
+video_path = os.path.join(IN_VIDEO_DIR, video_file)
 
 NMS_THRESH = 0.35
-SCORE_THRESH = [0.8, 0.55]
+SCORE_THRESH = [0.7, 0.5] #[0.8, 0.55]
 
 # --------------------------------------------------------------------------------------------------------------
 def press_q_to_quit(key):
@@ -51,7 +42,6 @@ def preprocess_video_frame_for_detector(frame, img_w, img_h, device):
     frame = frame.unsqueeze(0).to(device)
     return frame
 
-
 def draw_detections(frame, objclass, bboxs, resize_factor_w, resize_factor_h):
     bboxs[:, [0,2]] *= resize_factor_w
     bboxs[:, [1,3]] *= resize_factor_h
@@ -71,6 +61,7 @@ def draw_detections(frame, objclass, bboxs, resize_factor_w, resize_factor_h):
         br = (int(box[2]), int(box[3]))
         cv2.rectangle(frame, tl, br, (0,0,255), thickness=2)
     return frame
+
 
 
 def select_vehicle_detections(pred_score, pred_class, pred_box):
